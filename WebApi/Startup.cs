7f31fd002ca18f4fa9d89674aac76d4ea523baca
David Jiménez <david.jimenez@tokiota.com>
2016-04-18 17:43:06 +0200

    Add project files.

---
+++ WebApi/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Http;
+using Owin;
+
+namespace WebApi
+{
+    public static class Startup
+    {
+        // Este código configura Web API. La clase Startup se especifica como un parámetro de tipo
+        // en el método WebApp.Start.
+        public static void ConfigureApp(IAppBuilder appBuilder)
+        {
+            // Configurar Web API para autohospedaje. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
